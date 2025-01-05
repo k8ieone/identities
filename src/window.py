d71dbe83b0e84a03d8d51c22a2a943c631829bda
k8ie <k8ie@mcld.eu>
2025-01-05 18:22:26 +0100

    Initial concept

--- src/window.py
+++ src/window.py
@@ -19,12 +19,92 @@
 
 from gi.repository import Adw
 from gi.repository import Gtk
+from gi.repository import Gio
+from gi.repository import GLib
+
+from pathlib import Path
+import passpy
 
 @Gtk.Template(resource_path='/one/k8ie/Identities/window.ui')
 class IdentitiesWindow(Adw.ApplicationWindow):
     __gtype_name__ = 'IdentitiesWindow'
 
-    label = Gtk.Template.Child()
+    toolbarview = Gtk.Template.Child()
+    splitview = Gtk.Template.Child()
+    password_list_view = Gtk.Template.Child()
+
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.settings = Gio.Settings.new("one.k8ie.Identities")
+        self.builder = Gtk.Builder()
+        self.cur_dir = "."
+        print(self.settings.get_strv("stores"))
+        if len(self.settings.get_strv("stores")) > 0:
+            print("Skipping welcome page...")
+            self.toolbarview.set_content(self.splitview)
+        self.bind_actions()
+        self.store = passpy.store.Store()
+        self.generate_passwords_list()
+
+    def on_start_action(self, widget, _):
+        """Callback for the win.start action."""
+        if (Path.home() / ".password-store").is_dir():
+            print("Default password store detected")
+            self.settings.set_strv("stores", [".password-store"])
+            self.toolbarview.set_content(self.splitview)
+
+    def on_directory_action(self, widget, _):
+        """Callback for the win.directory action."""
+        print("Entering directory {}".format("{}".format(_.unpack())))
+        self.cur_dir = "{}".format(_.unpack())
+        self.generate_passwords_list()
+
+    def on_password_action(self, widget, _):
+        """Callback for the win.password action."""
+        print("Showing password {}".format(_))
+
+    def on_back_action(self, widget, _):
+        """Callback for the win.password action."""
+        parent = str(Path(self.cur_dir).parent)
+        self.cur_dir = parent
+        self.generate_passwords_list()
+
+    def generate_passwords_list(self):
+        dir_list = self.store.list_dir(self.cur_dir)
+        dirs = dir_list[0]
+        pwds = dir_list[1]
+        self.password_list_view.remove_all()
+        for entry in dirs:
+            button = Adw.ButtonRow(title=entry, action_name="win.directory", end_icon_name="go-next-symbolic")
+            button.set_action_target_value(GLib.Variant("s", entry))
+            self.password_list_view.append(button)
+        for entry in pwds:
+            button = Adw.ButtonRow(title=entry, action_name="win.password")
+            button.set_action_target_value(GLib.Variant("s", entry))
+            self.password_list_view.append(button)
+
+    def bind_actions(self):
+        actions = {
+            "start": {
+                "method": self.on_start_action,
+                "ret": None
+            },
+            "directory": {
+                "method": self.on_directory_action,
+                "ret": GLib.VariantType.new("s")
+            },
+            "password": {
+                "method": self.on_password_action,
+                "ret": GLib.VariantType.new("s")
+            },
+            "back": {
+                "method": self.on_back_action,
+                "ret": None
+            }
+        }
+        for action in actions:
+            act = Gio.SimpleAction.new(action, actions[action]["ret"])
+            print("Connecting {} to {}".format(action, actions[action]["method"]))
+            act.connect("activate", actions[action]["method"])
+            self.add_action(act)
