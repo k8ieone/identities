9d6cf1522b41afffe98f29e4fbac9819ffe6ccc8
k8ie <k8ie@mcld.eu>
2025-01-06 21:47:08 +0100

    Password view rework

--- src/window.py
+++ src/window.py
@@ -21,6 +21,7 @@ from gi.repository import Adw
 from gi.repository import Gtk
 from gi.repository import Gio
 from gi.repository import GLib
+from gi.repository import Gdk
 
 from pathlib import Path
 import passpy
@@ -29,12 +30,12 @@ import passpy
 class IdentitiesWindow(Adw.ApplicationWindow):
     __gtype_name__ = 'IdentitiesWindow'
 
+    clipboard = Gdk.Display.get_default().get_clipboard()
     toolbarview = Gtk.Template.Child()
     splitview = Gtk.Template.Child()
     password_list_view = Gtk.Template.Child()
-    password_view = Gtk.Template.Child()
-    password_box = Gtk.Template.Child()
-
+    password_page = Gtk.Template.Child()
+    password_group = Gtk.Template.Child()
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -48,12 +49,13 @@ class IdentitiesWindow(Adw.ApplicationWindow):
         self.bind_actions()
         self.store = passpy.store.Store(gpg_bin="gpg")
         self.generate_passwords_list()
+        self.password_group_children = []
 
     def on_start_action(self, widget, _):
         """Callback for the win.start action."""
         if (Path.home() / ".password-store").is_dir():
             print("Default password store detected")
-            self.settings.set_strv("stores", [".password-store"])
+            self.settings.set_strv("stores", ["~/.password-store"])
             self.toolbarview.set_content(self.splitview)
 
     def on_directory_action(self, widget, _):
@@ -64,16 +66,42 @@ class IdentitiesWindow(Adw.ApplicationWindow):
 
     def on_password_action(self, widget, _):
         """Callback for the win.password action."""
-        print("Showing password {}".format(_.unpack()))
-        content = self.store.get_key(_.unpack())
-        self.password_box.set_label(content)
-        self.splitview.set_content(self.password_view)
+        for child in self.password_group_children:
+            self.password_group.remove(child)
+        pwd_path = Path(_.unpack())
+        print("Showing password {}".format(pwd_path))
+        content = self.store.get_key(pwd_path)
+        self.password_group.set_title(str(pwd_path.parts[-1]))
+        self.password_group.set_description(str(pwd_path))
+        for index, line in enumerate(content.splitlines()):
+            row = Adw.ActionRow()
+            title = line
+            subtitle = None
+            if index == 0:
+                subtitle = "Password"
+            elif ": " in line:
+                subtitle = line.split(": ")[0]
+                title = line.removeprefix(subtitle + ": ")
+            row.set_title(title)
+            if subtitle is not None:
+                row.set_subtitle(subtitle)
+            copy_button = Gtk.Button(icon_name="edit-copy-symbolic", has_frame=False, valign=Gtk.Align(3), action_name="win.copy")
+            copy_button.set_action_target_value(GLib.Variant("s", title))
+            row.add_suffix(copy_button)
+            self.password_group_children.append(row)
+            self.password_group.add(row)
+        self.splitview.set_content(self.password_page)
 
     def on_back_action(self, widget, _):
         """Callback for the win.password action."""
         print("Back to directory {}".format("{}".format(self.cur_dir.parent)))
         self.cur_dir = self.cur_dir.parent
         self.generate_passwords_list()
+
+    def on_copy_action(self, widget, _):
+        """Callback for the win.password action."""
+        print("Copying to clipboard")
+        self.clipboard.set(_.unpack())
 
     def generate_passwords_list(self):
         dir_list = self.store.list_dir(self.cur_dir)
@@ -103,6 +131,10 @@ class IdentitiesWindow(Adw.ApplicationWindow):
             },
             "password": {
                 "method": self.on_password_action,
+                "ret": GLib.VariantType.new("s")
+            },
+            "copy": {
+                "method": self.on_copy_action,
                 "ret": GLib.VariantType.new("s")
             },
             "back": {
