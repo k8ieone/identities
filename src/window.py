bbede31a41d45b9d7e5c69d54f064d69c6dec18e
k8ie <k8ie@mcld.eu>
2025-01-12 23:10:56 +0100

    Closes #11; Initial OTP support

--- src/window.py
+++ src/window.py
@@ -25,6 +25,9 @@ from gi.repository import Gdk
 
 from pathlib import Path
 import passpy
+import pyotp
+import datetime
+import math
 import time
 
 @Gtk.Template(resource_path='/one/k8ie/Identities/window.ui')
@@ -51,12 +54,13 @@ class IdentitiesWindow(Adw.ApplicationWindow):
             self.set_content(self.splitview)
             self.brkpoint.connect("apply", self.on_collapse)
             self.brkpoint.connect("unapply", self.on_uncollapse)
+            #self.viewer_nav_view.connect("popped", self.page_hidden)
         self.bind_actions()
         self.store = passpy.store.Store(gpg_bin="gpg")
         page = self.build_navigation_page(self.cur_dir)
         self.browser_nav_view.add(page)
         self.generate_passwords_list()
-        self.password_group_children = []
+        self.otp_rows = {}
 
     def on_start_action(self, widget, _):
         """Callback for the win.start action."""
@@ -103,17 +107,91 @@ class IdentitiesWindow(Adw.ApplicationWindow):
         self.toast_overlay.add_toast(Adw.Toast(title="Entry copied to clipboard!", timeout=2))
         self.clipboard.set(_.unpack())
 
+    def map_value(self, value, from_min, from_max, to_min, to_max):
+        return to_min + (value - from_min) * (to_max - to_min) / (from_max - from_min)
+
+    def monitor_otp(self, row):
+        """Called by _task_internal_method. For some reason everything breaks if the UI is updated from here."""
+        time.sleep(1)
+
+    def otp_generated(self, window, task, whatevs):
+        """Callback - function called after monitor_otp finishes running"""
+        if not task.get_cancellable().is_cancelled():
+            task_data = task.get_task_data()
+            row = self.otp_rows[task_data]
+            expires_in = row.otp.interval - datetime.datetime.now().timestamp() % row.otp.interval
+            remapped = self.map_value(expires_in, 0, row.otp.interval, 0.0, 1.0)
+            row.bar.set_fraction(remapped)
+            row.set_text(row.otp.now())
+            new_task = self.create_new_task(self.otp_generated, row, id(row))
+            row.otp_task = new_task
+
+    def create_new_task(self, callback, row, row_id):
+        """Called whenever creating a new task. Starts _task_internal_method in a thread"""
+        task = Gio.Task.new(self, Gio.Cancellable(), callback, None)
+        task.set_return_on_cancel(False)
+        task.run_in_thread(self._task_internal_method)
+        task.set_task_data(row_id)
+        return task
+
+    def _task_internal_method (self, task, source_object, task_data, cancellable):
+        """Called by create_new_task in a thread"""
+
+        if task.return_error_if_cancelled():
+            task.return_value(None)
+        # Value of argument 'task_data' is always None for some reason.
+        # So, we need to get task_data using task.get_task_data() method.
+        task_data = task.get_task_data()
+
+        # Task data is just an id of the actual data. So, we need to get
+        # the actual data from our instance-wide dictionary
+        row = self.otp_rows[task_data]
+        outcome = self.monitor_otp(row)
+
+        task.return_value(outcome)
+
+    def password_page_shown(self, page):
+        otp_widgets = page.otp_rows
+        for widget in otp_widgets:
+            if widget.otp_task.get_cancellable().is_cancelled():
+                # Debug - to be removed
+                print("STARTING NEW TASK")
+                self.otp_rows[id(widget)] = widget
+                widget.otp_task = self.create_new_task(self.otp_generated, widget, id(widget))
+
+    def password_page_hidden(self, page):
+        otp_widgets = page.otp_rows
+        for widget in otp_widgets:
+            # Debug - to be removed
+            print("Cancelling task")
+            widget.otp_task.get_cancellable().cancel()
+            del self.otp_rows[id(widget)]
+
     def build_password_group(self, pwd_path):
         rel_path = pwd_path.relative_to(self.password_store_dir)
         content = self.store.get_key(str(rel_path).removesuffix(".gpg"))
-        password_group = Adw.PreferencesGroup()
+        password_group = Adw.PreferencesGroup(separate_rows=False)
         password_group.set_title(rel_path.stem)
         password_group.set_description(str(rel_path))
+        otp_rows = []
         for index, line in enumerate(content.splitlines()):
             row = Adw.PasswordEntryRow()
             text = line
             title = None
-            if index == 0:
+            otp = False
+            if text.startswith("otpauth://totp"):
+                title = "OTP"
+                otp = True
+                bar = Gtk.ProgressBar(inverted=False)
+                row.otp = pyotp.parse_uri(text)
+                expires_in = row.otp.interval - datetime.datetime.now().timestamp() % row.otp.interval
+                bar.set_fraction(self.map_value(expires_in, 0, row.otp.interval, 0.0, 1.0))
+                text = row.otp.now()
+                otp_rows.append(row)
+                self.otp_rows[id(row)] = row
+                row.otp_task = self.create_new_task(self.otp_generated, row, id(row))
+                row.bar = bar
+            elif index == 0:
                 title = "Password"
             elif ": " in line:
                 title = line.split(": ")[0]
@@ -140,8 +218,15 @@ class IdentitiesWindow(Adw.ApplicationWindow):
             copy_button = Gtk.Button(icon_name="edit-copy-symbolic", has_frame=False, valign=Gtk.Align(3), action_name="win.copy")
             copy_button.set_action_target_value(GLib.Variant("s", text))
             row.add_suffix(copy_button)
-            password_group.add(row)
-        return password_group
+            if otp:
+                another_box = Gtk.Box(orientation=Gtk.Orientation(1))
+                another_box.append(row)
+                another_box.append(bar)
+                another_row = Adw.ActionRow(child=another_box)
+                password_group.add(another_row)
+            else:
+                password_group.add(row)
+        return (password_group, otp_rows)
 
     def build_viewer_page(self, path):
         """Creates a new password viewer Adw.NavigationPage for a given entry."""
@@ -149,7 +234,8 @@ class IdentitiesWindow(Adw.ApplicationWindow):
         bar = Adw.HeaderBar()
         button = Gtk.Button(css_classes=["suggested-action", "pill"], label="Edit", action_name="win.edit")
         box = Gtk.Box(orientation=Gtk.Orientation(1), spacing=20)
-        box.append(self.build_password_group(pp))
+        group_tuple = (self.build_password_group(pp))
+        box.append(group_tuple[0])
         box.append(button)
         clamp = Adw.Clamp(maximum_size=450, child=box)
         status = Adw.StatusPage(child=clamp)
@@ -158,6 +244,9 @@ class IdentitiesWindow(Adw.ApplicationWindow):
         toolbarview.add_top_bar(bar)
         #print("Build page: {}".format(path))
         page = Adw.NavigationPage(title=path.stem, child=toolbarview)
+        page.connect("hidden", self.password_page_hidden)
+        page.connect("shown", self.password_page_shown)
+        page.otp_rows = group_tuple[-1]
         return page
 
     def build_passwords_list_box(self, directory):
