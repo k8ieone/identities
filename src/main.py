ecb0c4e8400c8d59d83c09cdc63238f5cee7e158
k8ie <k8ie@mcld.eu>
2025-01-15 21:11:17 +0100

    Switch to Adw.AboutDialog

--- src/main.py
+++ src/main.py
@@ -50,14 +50,13 @@ class IdentitiesApplication(Adw.Application):
 
     def on_about_action(self, widget, _):
         """Callback for the app.about action."""
-        about = Adw.AboutWindow(transient_for=self.props.active_window,
-                                application_name='Identities',
+        about = Adw.AboutDialog(application_name='Identities',
                                 application_icon='one.k8ie.Identities',
                                 developer_name='Kate',
                                 version='0.1.0',
                                 developers=['Kate'],
                                 copyright='© 2025 Kate')
-        about.present()
+        about.present(parent=self.props.active_window)
 
     def on_preferences_action(self, widget, _):
         """Callback for the app.preferences action."""
